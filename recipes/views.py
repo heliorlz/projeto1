ee794f3444c52eaae5dcc302f2b6065c9a3c3966
Helio Alves Vieira Junior <heleralvess@gmail.com>
2025-10-29 13:50:32 -0300

    Aplicando partials template e iniciando a página Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(
         request,
-        "recipes/home.html",
+        "recipes/pages/home.html",
         context={
             "name": "Curso Django Web e REST Framework",
         },
     )
-
-
-def contato(request):
-    return HttpResponse("Contato")
-
-
-def sobre(request):
-    return HttpResponse("Sobre")
