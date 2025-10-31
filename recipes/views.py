c92e3a6c834f941f1a416e6cb630fa72201824a1
Helio Alves Vieira Junior <heleralvess@gmail.com>
2025-10-31 10:01:54 -0300

    Criando um template base.html para extender em outras páginas

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,10 @@ def home(request):
         request,
         "recipes/pages/home.html",
     )
+
+
+def recipe(request, id):
+    return render(
+        request,
+        "recipes/pages/recipe-view.html",
+    )
