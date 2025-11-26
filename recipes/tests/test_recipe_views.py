cfb9b373770e815447e1e8e92c33b17913c86eed
Helio Alves Vieira Junior <heleralvess@gmail.com>
2025-11-26 13:17:02 -0300

    Iniciando a pagina de search

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -126,6 +126,11 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(view.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        url = reverse("recipes:search")
+        url = reverse("recipes:search") + "?q=teste"
         response = self.client.get(url)
         self.assertTemplateUsed(response, "recipes/pages/search.html")
+
+    def test_recipe_search_raises_404_if_no_query_provided(self):
+        url = reverse("recipes:search")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
